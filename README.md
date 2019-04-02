7d07b8bf6fe38da80d792f71fa2945ebba08a448
FerasTr <32588570+FerasTr@users.noreply.github.com>
2019-04-02 10:29:27 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Debug-script
+Bash script that automatically debugs C++/C files using valgrind and helgrind
+* **Run this script by: ./BasicCheck.sh \<path-to-file\> \<file-name\> \<arguments\>**
---
+++ TestFiles/main.cpp
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <cstdlib>
+#include <iostream>
+
+int var = 0;
+
+void* child_fn ( void* arg ) {
+	var++; /* Unprotected relative to parent */
+	return NULL;
+}
+
+int main(int argc, char** argv) {
+
+	std::cout << "You have entered " << argc 
+         << " arguments:" << "\n"; 
+  
+    for (int i = 0; i < argc; ++i) 
+        std::cout << argv[i] << "\n"; 
+
+	int *p =(int *)malloc(10 * sizeof(int));
+	free(p);
+	pthread_t child;
+	pthread_create(&child, NULL, child_fn, NULL);
+	var++; /* Unprotected relative to child */
+	pthread_join(child, NULL);
+	return 0;
+}
