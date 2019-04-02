b3bd77148b27a4a5785291a5d6be46bb5f368e5b
8HemLeT8 <44767602+8HemLeT8@users.noreply.github.com>
2019-04-02 10:38:07 +0300

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -26,39 +26,34 @@ printHel="PASS"
 
 # Check if Makefile exists
 cd $dirPath
-if [ ! -f Makefile ]; then
-    echo "MAKEFILE NOT FOUND"
-    exit 7
+# Run Makefile
+make
+outMake=$?
+
+if [ $outMake -gt 0 ]; then
+	finOut=$(($finOut + 7))
+	printMake="FAIL"
+	printVal="FAIL"
+	printHel="FAIL"
 else
-	# Run Makefile
-	make
-	outMake=$?
+	# Shift the arguments array by 2
+	shift 2
 
-	if [ $outMake -gt 0 ]; then
-		finOut=$(($finOut + 7))
-		printMake="FAIL"
+	# Run memory leak test
+	valgrind --leak-check=full --error-exitcode=1 ./$program $@ &>/dev/null
+	outVal=$?
+
+	if [ $outVal -eq 1 ]; then
+		finOut=$(($finOut + 2))
 		printVal="FAIL"
+	fi
+
+	# Run thread racing test
+	valgrind --tool=helgrind --error-exitcode=1 ./$program $@ &>/dev/null
+	outHel=$?
+	if [ $outHel -eq 1 ]; then
+		finOut=$(($finOut + 1))
 		printHel="FAIL"
-	else
-		# Shift the arguments array by 2
-		shift 2
-
-		# Run memory leak test
-		valgrind --leak-check=full --error-exitcode=1 ./$program $@ &>/dev/null
-		outVal=$?
-
-		if [ $outVal -eq 1 ]; then
-			finOut=$(($finOut + 2))
-			printVal="FAIL"
-		fi
-
-		# Run thread racing test
-		valgrind --tool=helgrind --error-exitcode=1 ./$program $@ &>/dev/null
-		outHel=$?
-		if [ $outHel -eq 1 ]; then
-			finOut=$(($finOut + 1))
-			printHel="FAIL"
-		fi
 	fi
 fi
 cd -
